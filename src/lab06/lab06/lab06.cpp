57813cb0c1a87c435aff4502cb78e0e2f786f794
tapokalypsis <73023100+tapokalypsis@users.noreply.github.com>
2021-03-22 10:12:16 +0200

    added labs 3, 5-7

---
+++ src/lab06/lab06/lab06.cpp
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+
+void show(int arr[5][5]);
+
+int main() {
+	int tmp;
+	int arr[5][5];
+	for (int i = 0; i < 5; i++) 
+	{
+		for (int j = 0; j < 5; j++) 
+		{
+			arr[i][j] = j + 1;
+		}
+	}
+	show(arr);
+	printf ("\n");
+
+	for (int i = 0; i < 5; i++) 
+	{
+		tmp = arr[i][0];
+		for (int j = 0; j < 4; j++) 
+		{
+			arr[i][j] = arr[i][j + 1];
+		}
+		arr[i][4] = tmp;
+	}
+	show(arr);
+
+	return 0;
+}
+
+void show(int arr[5][5]) {
+	for (int i = 0; i < 5; i++) 
+	{
+		for (int j = 0; j < 5; j++) 
+		{
+			printf( "%d", ( arr[i][j] ));
+		}
+		printf("\n");
+	}
+}
---
+++ src/lab07/lab07/lab07.cpp
@@ -0,0 +1,118 @@
+﻿/**
+ \file lab07.cpp
+ \brief Виконання лабораторної роботи 7.
+ \author Yakutenok Kostyantin
+ \version 1
+ \date 24.12.2020
+*/
+
+#include <stdio.h>
+#include <cmath>
+#include <stdarg.h>
+
+/**
+*Перевірка на просте число за допомогою циклу For.
+* @param digit : перевіряється на те, чи є простим числом.
+* @return result: повертає відповідь - просте число чи ні.
+*/
+bool CheckSimpleFor(int);
+
+/**
+*Перевірка на просте число за допомогою циклу While.
+* @param digit : перевіряється на те, чи є простим числом.
+* @return result: повертає відповідь - просте число чи ні.
+*/
+bool CheckSimpleWhile(int);
+
+/**
+*Перевірка на просте число за допомогою циклу DoWhile.
+* @param digit : перевіряється на те, чи є простим числом.
+* @return result: повертає відповідь - просте число чи ні.
+*/
+bool CheckSimpleDoWhile(int);
+
+/**
+ * Функція приймає різну кількість параметрів та перевіряє скільки попередніх чисел меньших від поточних
+ * @param amountOfArgument: кількість параметрів яку приймає функція
+ * @param ...: змінна кількість int параметрів для перевірки
+ * @return result: повертає відповідь - скільки попередніх чисел меньших від поточних
+ */
+int numericalPairСounter(int, ...);
+
+/**
+ * Головна функція з якої починається робота,запускаємо для перевірки функції за завданням
+ */
+int main(void)
+{
+	printf("%d \n ", numericalPairСounter(5, 1, 2, 3, 4, 5));
+	printf("%d", CheckSimpleFor(9));
+	return 0;
+}
+
+bool CheckSimpleFor(int number)
+{
+	bool conclusion = true;
+	for (int i = 17; i < number; i++)
+	{
+		if (number % i == 0)
+		{
+			conclusion = false;
+			break;
+		}
+	}
+
+	return conclusion;
+}
+
+bool CheckSimpleWhile(int number)
+{
+	bool conclusion = true;
+	int i = 17;
+	do
+	{
+		if (number % i == 0)
+		{
+			conclusion = false;
+			break;
+		}
+		i++;
+	} while (i < number);
+
+	return conclusion;
+}
+
+bool CheckSimpleDoWhile(int number) {
+	bool conclusion = true;
+	int i = 17;
+	while (i < number)
+	{
+		if (number % i == 0)
+		{
+			conclusion = false;
+			break;
+		}
+		i++;
+	}
+	return conclusion;
+}
+
+int numericalPairСounter(int amountOfArgument, ...)
+{
+	int result = 0;
+	va_list args;
+	va_start(args, amountOfArgument);
+
+	int* arr = new int[amountOfArgument];
+	for (int i = 0; i < amountOfArgument; ++i)
+	{
+		arr[i] = va_arg(args, int);
+	}
+	for (int i = 0; i < amountOfArgument - 1; i++) 
+	{
+		if (arr[i] < arr[i + 1])
+			result++;
+	}
+	va_end(args);
+
+	return result;
+}
